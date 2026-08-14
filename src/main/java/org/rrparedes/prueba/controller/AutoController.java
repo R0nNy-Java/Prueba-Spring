f6271a05faef01254a13591364950135bc4ddb2a
Rodny Paredes <rodnyparedesgg@gmail.com>
2026-08-14 10:50:07 -0500

    v 0.3 Prueba Con Viñetas y Ejemplos Nuev

--- src/main/java/org/rrparedes/prueba/controller/AutoController.java
+++ src/main/java/org/rrparedes/prueba/controller/AutoController.java
@@ -14,14 +14,14 @@ public class AutoController {
     @GetMapping({"/autos"})
     public String autos(Model model) {
 
-        Auto a1 = new Auto("PBA-1234", "totoya", 2022, "Rojo", 45000.5);
+        Auto a1 = new Auto("XBA-1010", "totoya Yaris", 2023, "Plateado", 12500.0);
 
         List<Auto> autos = new ArrayList<>();
         autos.add(a1);
-        autos.add(new Auto("PBB-5678", "Ford", 2020, "Negro", 78000.0));
-        autos.add(new Auto("PBC-9012", "Chevrolet", 2021, "Blanco", 60000.0));
-        autos.add(new Auto("PBD-3456", "Nissan", 2019, "Gris", 92500.0));
-        autos.add(new Auto("PBE-7890", "Hyundai", 2023, "Azul", 15000.0));
+        autos.add(new Auto("PCH-2020", "Chevrolet Sail", 2021, "Azul Marino", 48000.0));
+        autos.add(new Auto("FMD-3030", "Ford Mustang", 2022, "Amarillo", 25000.0));
+        autos.add(new Auto("KSP-4040", "Kia Sportage", 2024, "Blanco Perla", 8000.0));
+        autos.add(new Auto("MCX-5050", "Mazda CX-5", 2020, "Rojo Cereza", 62000.0));
 
         model.addAttribute("saludo", "Catálogo de Vehículos Registrados");
         model.addAttribute("auto", a1);
